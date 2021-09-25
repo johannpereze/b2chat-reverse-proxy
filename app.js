8d7be3368959753a39b7e33e021cdc0f8361ff17
johannpereze <sebas.jsp@gmail.com>
2021-09-25 10:47:49 -0500

    Configurado el puerto como variable de entorno

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 const { default: axios } = require("axios");
 const express = require("express");
 const { response, request } = require("express");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 require("dotenv").config();
 
-app.use(cors({
-  origin:'*'
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 // const API_ENDPOINT = process.env.API_ENDPOINT; //Creo que esto no me sirve como variable de entorno porque lo voy a hacer específico para b2chat y no necesito cambiarlo luego
 
-const port = 8084; //process.env.PORT;
-const LOGIN_HASH = process.env.LOGIN_HASH
+const port = process.env.PORT;
+const LOGIN_HASH = process.env.LOGIN_HASH;
 
 //Podría hacer 2 endpoints. Uno de autenticación y otro de broadcast
 
 app.get("/oauth/token", (req = request, res = response) => {
-
-//código de postman
+  //código de postman
   var config = {
-    method: 'post',
-    url: 'https://api.b2chat.io/oauth/token?grant_type=client_credentials',
-    headers: { 
-      'Content-Type': 'application/x-www-form-urlencoded', 
-      'Authorization': `Basic ${LOGIN_HASH}`
-    }
+    method: "post",
+    url: "https://api.b2chat.io/oauth/token?grant_type=client_credentials",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Basic ${LOGIN_HASH}`,
+    },
   };
-
-
-//Este es el codigo viejo
-  // axios
-  //   .get("https://pokeapi.co/api/v2/pokemon-species/4/") //aquí me falta pegarle el grant type. PONERLE POST. la ur real sería https://api.b2chat.io/oauth/token
-
 
   axios(config)
     .then((response) => {
-      res.json(response.data)
+      res.json(response.data);
     })
     .catch((err) => {
       console.log(err);
