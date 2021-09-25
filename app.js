f18b398f81eebc116b1963a1e58b40b456e6eeb3
johannpereze <sebas.jsp@gmail.com>
2021-09-25 10:02:48 -0500

    Estoy imprimiendo en pantalla la respuesta en un json

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-//env
+const { default: axios } = require("axios");
+const express = require("express");
+const { response, request } = require("express");
+const app = express();
+require("dotenv").config();
 
-const port = process.env.PORT
- 
-app.get(':endpoint([\\/\\w\\.-]*)', (req, res)=> { //Con esta expresión regular, no importa que ruta envíen siempre y cuando sea una ruta válida
-  res.send('Hello proxy')
-})
- 
-app.listen(port, ()=>{
-    console.log(`Listenin on port http://localhost:${port}`);
-}) 
+// const API_ENDPOINT = process.env.API_ENDPOINT; //Creo que esto no me sirve como variable de entorno porque lo voy a hacer específico para b2chat y no necesito cambiarlo luego
 
+const port = 8084; //process.env.PORT;
+
+//Podría hacer 2 endpoints. Uno de autenticación y otro de broadcast
+
+app.get("/oauth/token", (req = request, res = response) => {
+  //Con esta expresión regular, no importa que ruta envíen siempre y cuando sea una ruta válida :endpoint([\\/\\w\\.-]*)
+  // res.send("Hello world");
+
+  const { grant_type = "client_credentials" } = req.query;
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon-species/4/") //aquí me falta pegarle el grant type. la ur real sería https://api.b2chat.io/oauth/token
+    .then((response) => {
+      res.json(response.data.habitat)
+      // console.log(response.data.habitat);
+      console.log("grant_type", grant_type);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listenin on port http://localhost:${port}`);
+});
