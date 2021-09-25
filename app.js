97c94c0d2abce395c35a285e584b468a9464a1e6
johannpereze <sebas.jsp@gmail.com>
2021-09-24 20:10:44 -0500

    agrego regex para que cualquier dirección cargue el root de mi server express

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+require('dotenv').config()
+//env
 
 const port = process.env.PORT
  
-app.get('/', (req, res)=> {
+app.get(':endpoint([\\/\\w\\.-]*)', (req, res)=> { //Con esta expresión regular, no importa que ruta envíen siempre y cuando sea una ruta válida
   res.send('Hello proxy')
 })
  
